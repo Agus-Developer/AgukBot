55f6169590d72b6936442e43b1a4b6e7770f099d
VenixDeveloper_RLBX <venixsoftware3477@gmail.com>
2026-03-06 20:21:58 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,34 +1,7 @@
 import streamlit as st
 import groq
 
-Modelos = ['Seleccionar un modelo', 'llama-3.1-8b-instant', 'llama-3.3-70b-versatile', 'openai/gpt-oss-120b']
-
-def sidebar():
-    st.sidebar.title(":green[Menu General]")
-    m = st.sidebar.selectbox("Eleji el modelo que desee", Modelos, index=0)
-    st.sidebar.divider()
-    st.sidebar.markdown("Creado por :green-background[Agustin Aguirre]")
-    st.sidebar.markdown("Para :orange-background[TALENTO TECH]")
-
-    if m == "Seleccionar un modelo":
-        st.markdown("No se ha seleccionado ningun modelo :material/close:")
-    elif m == "llama-3.1-8b-instant":
-        st.markdown("Modelo Seleccionado: :orange[llama-3.1-8b-instant] :material/smart_toy:")
-    elif m == "llama-3.3-70b-versatile":
-        st.markdown("Modelo Seleccionado: :green[llama-3.3-70b-versatile] :material/smart_toy:")
-    elif m == "openai/gpt-oss-120b":
-        st.markdown("Modelo Seleccionado: :violet[openai/gpt-oss-120b] :material/smart_toy:")
-    st.divider()
-    return m
-
-def config():
-    st.set_page_config(
-        page_title="AgukBot", 
-        page_icon="🤖",
-        layout="centered")
-    st.title(":green[Agukbot:]:violet[   Chatbot IA 🤖]")
-    st.markdown(":small[:green[AgukBot] es mi primer proyecto personal creado con Python. Este chatbot abarca dos modelos: :violet[Llama3 y Mixtral]]")
-    st.divider()
+Modelos = ['Seleccionar un modelo', 'openai/gpt-oss-120b', 'llama-3.3-70b-versatile', 'qwen/qwen3-32b']
 
 def client():
     groqkey = st.secrets["GROQ_API_KEY"]
@@ -37,7 +10,7 @@ def client():
 def init_state_chat():
     if "mensajes" not in st.session_state:
         st.session_state.mensajes = []
-        
+
 def prompt_input():
     return st.chat_input("Enviar un Mensaje")
 
@@ -74,11 +47,53 @@ def ejecute_app():
         message_history("user", mensaje_user)
         mostrar_mensaje("user", mensaje_user)
 
+        if modelo == "Seleccionar un modelo":
+            mostrar_mensaje("assistant", "No se ha seleccionado ningun modelo :material/close:")
+
         respuestas_modelo = obtener_respuesta_GROQ(cliente, modelo, st.session_state.mensajes)
 
         message_history("assistant", respuestas_modelo)
 
         mostrar_mensaje("assistant", respuestas_modelo)
+
+def sidebar():
+    st.sidebar.title(":green[Menu General]")
+    m = st.sidebar.selectbox("Elegi el modelo que desee", Modelos, index=0)
+    st.sidebar.divider()
+    st.sidebar.markdown("Creado por :green-background[Agustin Aguirre]")
+    st.sidebar.markdown("Para :orange-background[TALENTO TECH | GCBA]")
+    
+    if m.startswith("openai/gpt-oss-120b"):
+        st.sidebar.markdown(":orange[**openai/gpt-oss-120b es un modelo de lenguaje desarrollado por OpenAI, diseñado para ofrecer respuestas rápidas y eficientes con un tamaño de 8 mil millones de parámetros.**]")
+    elif m.startswith("llama-3.3-70b-versatile"):
+        st.sidebar.markdown(":green[**Llama 3.3-70b-versatile es un modelo de lenguaje desarrollado por Meta, diseñado para ofrecer respuestas versátiles y eficientes con un tamaño de 70 mil millones de parámetros.**]")
+    elif m.startswith("qwen/qwen3-32b"):
+        st.sidebar.markdown(":violet[**Qwen3-32b es un modelo de lenguaje desarrollado por Alibaba, diseñado para ofrecer respuestas precisas y eficientes con un tamaño de 32 mil millones de parámetros.**]")
+
+
+    if m == "Seleccionar un modelo":
+        st.markdown("No se ha seleccionado ningun modelo :material/close:")
+        st.session_state.mensajes = []
+    elif m == "openai/gpt-oss-120b":
+        st.markdown("Modelo Seleccionado: :orange[openai/gpt-oss-120b] :material/smart_toy:")
+        st.session_state.mensajes = []
+    elif m == "llama-3.3-70b-versatile":
+        st.markdown("Modelo Seleccionado: :green[llama-3.3-70b-versatile] :material/smart_toy:")
+        st.session_state.mensajes = []
+    elif m == "qwen/qwen3-32b":
+        st.markdown("Modelo Seleccionado: :violet[qwen/qwen3-32b] :material/smart_toy:")
+        st.session_state.mensajes = []
+    st.divider()
+    return m
+
+def config():
+    st.set_page_config(
+        page_title="AgukBot", 
+        page_icon="🤖",
+        layout="centered")
+    st.title(":green[Agukbot:]:violet[   Chatbot IA 🤖]")
+    st.markdown(":small[:green[AgukBot] es mi primer proyecto personal creado con Python. Este chatbot abarca tres modelos: :blue[openai/gpt-oss-120b, Llama 3.3 y Qwen3-32b]. El objetivo de este proyecto es aprender a integrar modelos de lenguaje en aplicaciones prácticas, y explorar las capacidades de la inteligencia artificial en el procesamiento del lenguaje natural. :material/smart_toy:]")
+    st.divider()
 
 if __name__ == "__main__":
     ejecute_app()
